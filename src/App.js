267bbc781d33c8e25cd400143e5575de0ffe5f9e
TripKings <antonio.cicchese@gmail.com>
2020-03-05 19:52:08 -0600

    starting on recipe app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App  = () => {
+
+  const APP_ID = process.env.APP_ID;
+  const APP_KEY = process.env.APP_KEY;
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+
+  return(
+    <div className= "App"> 
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
 export default App;
