1948ce6b1ca01494f530001c579043b3b00453ad
TripKings <antonio.cicchese@gmail.com>
2020-04-16 13:41:40 -0500

    working on my final project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
+
 import './App.css';
 
 const App  = () => {
 
-  const APP_ID = process.env.APP_ID;
-  const APP_KEY = process.env.APP_KEY;
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  const REACT_APP_API_ID = process.env.REACT_APP_API_ID;
+  const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken')
+
+  useEffect (() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_API_ID}&app_key=${REACT_APP_API_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits)
+
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
 
   return(
     <div className= "App"> 
-      <h1>Hello</h1>
+      <form onSubmit={getSearch} className= "search-form">
+        <input className= "search-bar" type= "text" value={search} onChange={updateSearch} />
+        <button className="search-button" type= "submit">Search</button>
+      </form>
+      <div className='recipes'>
+      {recipes.map(recipe => (
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+
+        />
+        
+      ))}
+      </div>
     </div>
   )
 }
